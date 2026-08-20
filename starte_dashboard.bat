5683a17d396ddcb8e9d31637cdb7c18e807bc289
Christian Schmitt <ipsschmitt@gmail.com>
2026-08-20 09:35:22 +0200

    Add starte_dashboard.bat - CMM 8.0

---
+++ starte_dashboard.bat
@@ -0,0 +1,11 @@
+@echo off
+cd /d "%~dp0"
+echo ================================================================
+echo Code_Maker_Matrix (CMM) v6.0 Enterprise
+echo (c) 2026 Christian Schmitt, Solingen, Germany
+echo Email: c.schmitt@me.com
+echo Tel.: 015204006286
+echo ================================================================
+echo.
+echo Starte CMM...
+streamlit run dashboard.py
